fe980a56a3de51e13be31f839f1fd5d6567ec58f
AnnKemp <ann.kemp@scarlet.be>
2021-10-06 22:53:38 +0200

    Constants . . .

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,10 +10,18 @@ namespace HelloWorld
             Console.WriteLine("I will print on a new line.");
             Console.Write("Hello World! ");
             Console.Write("I will print on the same line.");
-            // This is a comment!
+            // This is a comment! 
+            /* variables have types: int, bool, double, char and string */
             int myNum;
             myNum = 10;
+            myNum = 20;
             Console.WriteLine(myNum);
+            string name = "John";
+            Console.WriteLine(name);
+            // constants are read only after they've initialised
+            const int TheNum = 50;
+            Console.WriteLine(TheNum);
+
         }
     }
 }

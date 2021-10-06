756b7efcda5c4bb94a4330a7c43aff7e5d9f7878
AnnKemp <ann.kemp@scarlet.be>
2021-10-06 22:45:13 +0200

    use of WriteLine and Console.Write && variables

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,6 +7,10 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("I will print on a new line.");
+            Console.Write("Hello World! ");
+            Console.Write("I will print on the same line.");
+            // This is a comment!
         }
     }
 }
